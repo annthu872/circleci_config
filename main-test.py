f3975ce4ac57295dbb215408f07ff72c84a9296f
anhthu872 <88284745+annthu872@users.noreply.github.com>
2023-07-09 03:11:51 +0700

    Update main-test.py

--- main-test.py
+++ main-test.py
@@ -2,7 +2,7 @@ from main import Add
 
 def Add_test():
     assert Add(2,3) == 5
-    assert Add(4,6) == 100
+    assert Add(4,6) == 10
     print("Test Add Function Success")
 
 if __name__ =="__main__":
