3f811c3b211911c95c4e6fe57bf9882df9e3b477
anhthu872 <88284745+annthu872@users.noreply.github.com>
2023-07-09 23:33:37 +0700

    test

--- test_calculator.py
+++ test_calculator.py
@@ -7,7 +7,7 @@ def Add_test():
     assert Add(10,-5) == 5
     assert Add(3.14,1.86) ==5.0
     assert Add(1000000,1) ==1000001
-    assert Add(100,2000) == 300
+    assert Add(100,200) == 300
     assert Add(-10,-20) ==-30
     assert Add(0.5,0.5) == 1.0
     assert Add(9999999999999999,1) == 10000000000000000
